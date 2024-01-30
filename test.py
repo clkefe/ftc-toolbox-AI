dcaf4ceea96f98d9e8f6558155777a416c5550cb
Efe <suleymanefeclk2@gmail.com>
2024-01-30 11:36:20 -0800

    add: exmaple team stat function call

--- test.py
+++ test.py
@@ -1,6 +1,30 @@
 from ollama import generate
 
-PROMPT = '''
+PROMPT = '''    
+Function:
+def get_team_id(team_name: str):
+  """
+  Returns the First Tech Challenge team's ID by searching in the vector database
+
+  Args:
+     team_name: The First Tech Challenge (FTC) team name that is provided in the user query.
+     
+  Returns:
+    int: The FTC team's ID
+  """
+    
+Function:
+def get_team_stats(team_id: int):
+  """
+  Returns the stats for a First Tech Challenge team. Call this only when the user query specifically asks for a team's stats. 
+
+  Args:
+     team_id: The First Tech Challenge (FTC) team ID
+     
+  Returns:
+    string: the stats of the FTC team
+  """
+  
 Function:
 def get_relevant_docs(user_query: str):
     """
@@ -14,7 +38,7 @@ def get_relevant_docs(user_query: str):
     """
     
 Function:
-def no_relevant_function(user_query : str):
+def no_relevant_function(user_query: str):
   """
   Call this when no other provided function can be called to answer the user query.
 
@@ -22,8 +46,10 @@ def no_relevant_function(user_query : str):
      user_query: The user_query that cannot be answered by any other function calls.
   """
 
-User Query: How is the weather in Istanbul?<human_end>
+User Query: How is the Huskyteers doing this season?<human_end>
 '''
+
+# Ex user query: What is considered as knocking down the pixels during the driver controlled period?
 
 response = generate('TEST_FUNCTION_CALLING', PROMPT)
 ai_output = response['response']
